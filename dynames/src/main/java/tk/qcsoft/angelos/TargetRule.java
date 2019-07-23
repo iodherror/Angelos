68597252a05a193d2fbbcb2de4eaf08986edfbe0
qiucheng <qiucheng@dpandora.cn>
2019-07-23 10:07:09 +0800

    Dynames增加了IN,NOT_IN逻辑

--- dynames/src/main/java/tk/qcsoft/angelos/TargetRule.java
+++ dynames/src/main/java/tk/qcsoft/angelos/TargetRule.java
@@ -2,7 +2,10 @@ package tk.qcsoft.angelos;
 
 import tk.qcsoft.angelos.constant.Operation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Created by QC on 2019/7/22 15:16.
@@ -10,15 +13,23 @@ import java.util.Objects;
 public class TargetRule {
 
     private Operation operation;
-    private String value;
+    private ArrayList<String> valueList;
 
     private TargetRule() {
 
     }
 
-    private TargetRule(String value, Operation operation) {
+    private TargetRule(Operation operation,String value) {
         Objects.requireNonNull(value);
-        this.value = value;
+        this.valueList = new ArrayList<>();
+        valueList.add(value);
+        this.operation = operation;
+    }
+
+    private TargetRule(Operation operation, String... value) {
+        Objects.requireNonNull(value);
+        this.valueList = new ArrayList<>();
+        valueList.addAll(Arrays.stream(value).collect(Collectors.toList()));
         this.operation = operation;
     }
 
@@ -27,26 +38,34 @@ public class TargetRule {
     }
 
     public Boolean test(String target) {
-        if (operation.equals(Operation.SAME)) return value.equals(target);
-        if (operation.equals(Operation.LIKE)) return target.length() > 0 && target.contains(value);
-        if (operation.equals(Operation.NOT)) return !value.equals(target);
+        if (operation.equals(Operation.SAME)) return valueList.get(0).equals(target);
+        if (operation.equals(Operation.LIKE)) return target.length() > 0 && target.contains(valueList.get(0));
+        if (operation.equals(Operation.NOT)) return !valueList.get(0).equals(target);
         if (operation.equals(Operation.ANY)) return true;
+        if (operation.equals(Operation.IN)) return valueList.contains(target);
+        if (operation.equals(Operation.NOT_IN)) return !valueList.contains(target);
         throw new UnsupportedOperationException("invalid operation:" + operation.name());
     }
 
     public static TargetRule SAME(String value) {
-        return new TargetRule(value, Operation.SAME);
+        return new TargetRule(Operation.SAME,value);
     }
 
     public static TargetRule LIKE(String value) {
-        return new TargetRule(value, Operation.LIKE);
+        return new TargetRule(Operation.LIKE,value);
     }
 
     public static TargetRule NOT(String value) {
-        return new TargetRule(value, Operation.NOT);
+        return new TargetRule(Operation.NOT,value);
     }
 
     public static TargetRule ANY() {
         return new TargetRule(Operation.ANY);
+    }
+    public static TargetRule IN(String... value) {
+        return new TargetRule( Operation.IN,value);
+    }
+    public static TargetRule NOT_IN(String... value) {
+        return new TargetRule( Operation.NOT_IN,value);
     }
 }
--- dynames/src/main/java/tk/qcsoft/angelos/constant/Operation.java
+++ dynames/src/main/java/tk/qcsoft/angelos/constant/Operation.java
@@ -4,5 +4,5 @@ package tk.qcsoft.angelos.constant;
  * Created by QC on 2019/7/22 15:30.
  */
 public enum Operation{
-    SAME,LIKE,NOT,ANY
+    SAME,LIKE,NOT,ANY,IN,NOT_IN
 }
